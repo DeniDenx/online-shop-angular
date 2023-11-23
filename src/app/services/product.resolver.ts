d7db21860b6800b98cc2eaad42418c314cfd6d1b
Denis Bulkin <denidenx@yandex.ru>
2023-11-23 13:14:56 +0500

    angular update 15 to 16

--- src/app/services/product.resolver.ts
+++ src/app/services/product.resolver.ts
@@ -1,17 +1,13 @@
 import { ProductsService } from './products.service';
 import { IProducts } from './../models/products';
 import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
+import { Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { catchError, EMPTY, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolver implements Resolve<IProducts> {
+export class ProductResolver  {
   constructor(private ProductsService: ProductsService, private router: Router) { }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProducts> {
     return this.ProductsService.getProduct(route.params?.['id']).pipe(
